22435768e30132faa2cf5be8b5b9f132140acea4
Mythendros <129041223+Mythendros@users.noreply.github.com>
2024-03-21 21:12:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,32 @@
+import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fhand:
+    long_description = fhand.read()
+
+setuptools.setup(
+    name="primefaclearner-cli",
+    version="0.0.1",
+    author="Mythendros & lunatics",
+    author_email="amogus@web.de",
+    description=("An Cli-Tool to help you memorize prime factorisations "
+                "demonstrate python module and tool packaging."),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Mythendros/primefaclearner",
+    project_urls={
+        "Bug Tracker": "",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "primefaclearner = primefaclearner.cli:main",
+        ]
+    }
+)   
