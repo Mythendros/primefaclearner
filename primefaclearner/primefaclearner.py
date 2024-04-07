98eb78bd0f0db10b16f1b87581ee0a5c2d98fb2c
Mythendros <129041223+Mythendros@users.noreply.github.com>
2024-04-07 16:40:01 +0200

    Update primefaclearner.py

--- primefaclearner/primefaclearner.py
+++ primefaclearner/primefaclearner.py
@@ -75,7 +75,7 @@ def quizme(num1, num2):
                 primefac_answer[int(x)] = 1
             else:
                 x = x.split("^")
-                if bool(x) == True:
+                if not x:
                     primefac_answer[int(x[0])] = int(x[1])
         # Check if user's answer matches the precomputed prime factorization
         if primefac_answer == primefaclist[randomnumber-2]:
